c493a398cbd40079d1c1ab2efd1fb8d62ac6865f
Pavol Hejny <me@pavolhejny.com>
2022-05-07 22:24:28 +0200

    Using images in the tray

--- src/sample-tray-module.tsx
+++ src/sample-tray-module.tsx
@@ -1,6 +1,25 @@
 import { declareModule, makeTraySimpleModule, React } from '@collboard/modules-sdk';
 import { contributors, description, license, name, repository, version } from '../package.json';
 
+import img20220101_155315 from '../assets/20220101_155315.jpg';
+import img20220105_130407 from '../assets/20220105_130407.jpg';
+import img20220207_141122 from '../assets/20220207_141122.jpg';
+import img20220208_145707 from '../assets/20220208_145707.jpg';
+import img20220215_145915 from '../assets/20220215_145915.jpg';
+import img20220215_160201 from '../assets/20220215_160201.jpg';
+import img20220219_155700 from '../assets/20220219_155700.jpg';
+import img20220223_164612 from '../assets/20220223_164612.jpg';
+import img20220319_160701 from '../assets/20220319_160701.jpg';
+import img20220319_164131 from '../assets/20220319_164131.jpg';
+import img20220319_174200 from '../assets/20220319_174200.jpg';
+import img20220319_174757 from '../assets/20220319_174757.jpg';
+import img20220322_154653 from '../assets/20220322_154653.jpg';
+import img20220322_163319 from '../assets/20220322_163319.jpg';
+import img20220326_142552 from '../assets/20220326_142552.jpg';
+import img20220327_122308 from '../assets/20220327_122308.jpg';
+import img20220328_152716 from '../assets/20220328_152716.jpg';
+import img20220328_194840 from '../assets/20220328_194840.jpg';
+
 declareModule(
     makeTraySimpleModule({
         manifest: {
@@ -14,34 +33,40 @@ declareModule(
 
         icon: {
             order: 60,
-            icon: '📤' /* TODO: !!! Use icon as URL in assets */,
+            icon: img20220101_155315,
         },
         trayDefinition: [
             {
                 // TODO: !!! Mix internal and external assets + NON-image assets
                 title: 'Czechia',
-                icon: 'https://collboard.fra1.cdn.digitaloceanspaces.com/assets/18.42.0/languages/cs.svg',
+                icon: img20220101_155315,
                 groups: [
                     {
-                        title: <>Kraje</>,
+                        title: <>Images with</>,
                         items: [
-                            {
-                                //title: 'Moravskoslezsky kraj',
-                                imageSrc: `https://www.h-edu.cz/media/Ilustrace/illustration-mino-hedu.svg`,
-                                //artSrc: 'https://raw.githubusercontent.com/collboard/zip-support/main/samples/simple/sample.txt',
-                            },
-                            {
-                                //title: 'Moravskoslezsky kraj',
-                                imageSrc: `https://www.h-edu.cz/media/foto/h-edu-team.jpg`,
-                                //artSrc: 'https://raw.githubusercontent.com/collboard/zip-support/main/samples/simple/sample.txt',
-                            },
-                            {
-                                //title: 'Moravskoslezsky kraj',
-                                imageSrc: `https://raw.githubusercontent.com/collboard/zip-support/main/assets/hello-world-icon.png`,
-                                //artSrc: 'https://raw.githubusercontent.com/collboard/zip-support/main/samples/simple/sample.txt',
-                            },
+                            { imageSrc: img20220101_155315 },
+                            { imageSrc: img20220208_145707 },
+                            { imageSrc: img20220219_155700 },
+                            { imageSrc: img20220319_164131 },
+                            { imageSrc: img20220322_154653 },
+                            { imageSrc: img20220327_122308 },
+                            { imageSrc: img20220105_130407 },
+                            { imageSrc: img20220215_145915 },
+                            { imageSrc: img20220223_164612 },
+                            { imageSrc: img20220319_174200 },
+                            { imageSrc: img20220322_163319 },
+                            { imageSrc: img20220328_152716 },
+                            { imageSrc: img20220207_141122 },
+                            { imageSrc: img20220215_160201 },
+                            { imageSrc: img20220319_160701 },
+                            { imageSrc: img20220319_174757 },
+                            { imageSrc: img20220326_142552 },
+                            { imageSrc: img20220328_194840 },
                         ],
                     },
+
+                    /*
+                    !!! Remove
                     {
                         title: 'Kraje 2',
                         items: [
@@ -82,8 +107,12 @@ declareModule(
                             },
                         ],
                     },
+                    */
                 ],
             },
+
+            /*
+            !!! Delete and make new group
             {
                 title: 'Slovakia',
                 icon: 'https://collboard.fra1.cdn.digitaloceanspaces.com/assets/18.42.0/languages/sk.svg',
@@ -129,7 +158,7 @@ declareModule(
                         ],
                     },
                 ],
-            },
+            },*/
         ],
     }),
 );
